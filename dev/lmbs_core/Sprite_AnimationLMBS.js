1144be8d3b42fc2095ded2e032204e9a3961a69c
Ralph-VX <feizhaodan@hotmail.com>
2017-10-30 21:00:40 +0900

    Working around for new JSON format

--- dev/lmbs_core/Sprite_AnimationLMBS.js
+++ dev/lmbs_core/Sprite_AnimationLMBS.js
@@ -5,6 +5,10 @@
 // Extends from Sprite_Animation, with ability to load json timing and
 // process it.
 
+/*
+    
+*/
+
 function Sprite_AnimationLMBS() {
     this.initialize.apply(this, arguments);
 }
@@ -14,41 +18,47 @@ Sprite_AnimationLMBS.prototype.constructor = Sprite_AnimationLMBS;
 
 Sprite_AnimationLMBS.prototype.initialize = function(object, sprite){
     Sprite_Animation.prototype.initialize.call(this);
-    var parameters = object.parameters;
-    //                    origin,    dx,         dy,    jsoname   id,  delay,      mirror,          follow
-    if (parameters.match(/(.+?)\,([+-]?\d+)\,([+-]?\d+)\,(.*?)\,(\d+)\,(\d+)\,(true|false|null)\,(true|false)/)){
-        var origin = RegExp.$1;
-        var dx = parseInt(RegExp.$2,10);
-        var dy = parseInt(RegExp.$3,10);
-        var filename = RegExp.$4;
-        if (filename.length > 0) {
-            var obj = Kien.LMBS_Core.createAnimationTimingFromName(filename);
-        }
-        var animationId = parseInt(RegExp.$5,10);
-        var delay = parseInt(RegExp.$6,10);
-        var mirror = eval(RegExp.$7);
-        var follow = eval(RegExp.$8);
-    }
-    this._timingArray = obj || {};
-    this._originName = origin || "target";
-    this._dx = dx || 0;
-    this._dy = dy || 0;
-    this._animation = $dataAnimations[animationId] || null;
-    this._delay = delay || 0;
-    this._mirror = mirror
-    this._follow = follow || false;
-    this._targetSprite = null;
+    this._isLoaded = false;
+    this._targetSprite =  sprite.targetSprite();
+    this._target = this._targetSprite;
     this._finish = false;
     this._userSprite = sprite;
     this._battler = sprite._battler;
-    this._targetSprite =  sprite.targetSprite();
+    this._action = new Game_Action(this._battler);
+    this._action.setItemObject(object.item);
+    var name = object.parameters;
+    var xhr = new XMLHttpRequest();
+    var url = "data/animations/" + name + ".json";
+    xhr.open('GET', url, false);
+    xhr.overrideMimeType('application/json');
+    xhr.onload = function() {
+        if (xhr.status < 400) {
+            obj = JSON.parse(xhr.responseText);
+            this.onJSONloaded(obj);
+        }
+    }.bind(this); 
+    xhr.onerror = function() {
+        DataManager._errorUrl = DataManager._errorUrl || url;
+    };
+    xhr.send();
+}
+
+Sprite_AnimationLMBS.prototype.onJSONloaded = function(param) {
+    var thisObject = this._battler.getEvaluateObjects();
+    this._timingArray = param.timing || {};
+    this._xOrigin = param.x.origin || "target";
+    this._yOrigin = param.y.origin || "target";
+    this._dx = Kien.LMBS_Core.loadJSONEvaluableValue(param.x.value,this) || 0;
+    this._dy = Kien.LMBS_Core.loadJSONEvaluableValue(param.y.value,this) || 0;
+    this._animation = $dataAnimations[param.animationId] || null;
+    this._delay = param.delay || 0;
+    this._mirror = param.mirror || false
+    this._follow = param.follow || false;
     this._animationPosition = {
         "x" : this.animationX(),
         "y" : this.animationY(),
         "height" : this._targetSprite.height
     }
-    this._action = new Game_Action(this._battler);
-    this._action.setItemObject(object.item);
     if (this._mirror === null) {
         this._mirror = !this._battler._facing;
     }
@@ -60,6 +70,7 @@ Sprite_AnimationLMBS.prototype.initialize = function(object, sprite){
     } else {
         this._finish = true;
     }
+    this._isLoaded = true;
 }
 
 Sprite_AnimationLMBS.prototype.updateCellSprite = function(sprite, cell) {
@@ -94,25 +105,29 @@ Sprite_AnimationLMBS.prototype.initMembers = function() {
 
 
 Sprite_AnimationLMBS.prototype.animationX = function() {
-    switch (this._originName) {
+    switch (this._xOrigin) {
         case "target":
             return (this._targetSprite._battler.screenX() + this._dx);
         case "user":
             return (this._userSprite._battler.screenX() + this._dx);
         case "screen":
             return (this._dx);
+        case "field":
+            return (Kien.LMBS_Core.fieldToScreenX(this._dy));
     }
     return 0;
 }
 
 Sprite_AnimationLMBS.prototype.animationY = function() {
-    switch (this._originName) {
+    switch (this._yOrigin) {
         case "target":
-            return (this._targetSprite._battler.screenY() + this._dy);
+            return (this._targetSprite._battler.screenY() - this._dy);
         case "user":
-            return (this._userSprite._battler.screenY() + this._dy);
+            return (this._userSprite._battler.screenY() - this._dy);
         case "screen":
             return (this._dy);
+        case "field":
+            return (Kien.LMBS_Core.fieldToScreenY(this._dy));
     }
     return 0;
 }
@@ -123,13 +138,15 @@ Sprite_AnimationLMBS.prototype.removeLMBS = function() {
 
 
 Sprite_AnimationLMBS.prototype.updateMain = function() {
-    Sprite_Animation.prototype.updateMain.call(this);
-    this.updateTestData();
-    if (this.isPlaying() && this.isReady() && this._delay == 0) {
-        this.updateDamage();
-    }
-    if(!this.isPlaying()){
-        this._finish = true;
+    if (this._isLoaded) {
+        Sprite_Animation.prototype.updateMain.call(this);
+        this.updateTestData();
+        if (this.isPlaying() && this.isReady() && this._delay == 0) {
+            this.updateDamage();
+        }
+        if(!this.isPlaying()){
+            this._finish = true;
+        }
     }
 }
 
@@ -154,7 +171,7 @@ Sprite_AnimationLMBS.prototype.updateDamage = function() {
             this._action.apply(enemy._battler);
             var dir = obj.knockdir ? (this._battler._facing ? 4 : 6) : (this._battler._facing ? 6 : 4);
             if (this._action.isDamage() || this._action.isDrain()){
-                enemy._battler.knockback(obj.knockback,dir);
+                enemy._battler.knockback(obj.knockback,dir, obj.knocklength);
                 enemy._battler.onHitted(this._battler);
                 this._battler.onHit(enemy._battler);
             }
@@ -182,11 +199,20 @@ Sprite_AnimationLMBS.prototype.updatePosition = function() {
     }
     this.x = this._animationPosition.x;
     this.y = this._animationPosition.y;
-    if (this._animation.position == 0){
-        this.y -= this._animationPosition.height;
-    }
-    if (this._animation.position == 1){
-        this.y -= this._animationPosition.height/2
+    if (this._yOrigin == "field" || this._yOrigin == "screen") {
+        // if (this._animationPosition == 0) {
+        //     this.anchor.y = 0;
+        // }
+        // if (this._animationPosition == 2) {
+        //     this.anchor.y = 1;
+        // }
+    } else {
+        if (this._animation.position == 0){
+            this.y -= this._animationPosition.height;
+        }
+        if (this._animation.position == 1){
+            this.y -= this._animationPosition.height/2
+        }
     }
 
 };
@@ -205,11 +231,13 @@ Sprite_AnimationLMBS.prototype.updateTiming = function() {
         var array = this._timingArray[index];
         for (var i = 0; i < array.length;i++){
             var obj = Object.create(array[i]);
-            obj.hitted = [];
-            if (obj.knockdir == 0){
-                obj.knockdir = this._battler._facing ? 6 : 4;
+            if (obj.type == "damage") {
+                obj.hitted = [];
+                if (obj.knockdir == 0){
+                    obj.knockdir = this._battler._facing ? 6 : 4;
+                }
+                this._processingTiming.push(obj);
             }
-            this._processingTiming.push(obj);
         }
     }
 }
